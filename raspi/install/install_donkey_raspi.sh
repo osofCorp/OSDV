40785f9428b83914694167980ce26d4711cb14b5
44401213SeonghunPark <41362677+44401213SeonghunPark@users.noreply.github.com>
2023-12-05 22:51:06 +0900

    1.1

--- raspi/install/install_donkey_raspi.sh
+++ raspi/install/install_donkey_raspi.sh
@@ -9,10 +9,6 @@ sudo raspi-config nonint do_vnc 0
 sudo raspi-config nonint do_onewire 0
 sudo raspi-config nonint do_camera 0
 
-#Update and Upgrade
-yes | sudo apt-get update
-yes | sudo apt-get upgrade
-
 #Install Dependencies
 yes | sudo apt-get install build-essential python3 python3-dev python3-pip python3-virtualenv python3-numpy python3-picamera python3-pandas python3-rpi.gpio i2c-tools avahi-utils joystick libopenjp2-7-dev libtiff5-dev gfortran libatlas-base-dev libopenblas-dev libhdf5-serial-dev git ntp
 
