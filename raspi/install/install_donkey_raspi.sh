3cece4b3b7b3304597a7aab17b226eb2c4ddd19e
44401213SeonghunPark <41362677+44401213SeonghunPark@users.noreply.github.com>
2023-12-07 07:34:58 +0900

    1.3

--- raspi/install/install_donkey_raspi.sh
+++ raspi/install/install_donkey_raspi.sh
@@ -8,8 +8,8 @@ yes | sudo apt-get update
 yes | sudo apt-get upgrade
 
 
-sudo apt-get install build-essential cmake unzip pkg-config
-sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
+yes | sudo apt-get install build-essential cmake unzip pkg-config
+yes | sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
 
 #Install Dependencies
 yes | sudo apt-get install build-essential python3 python3-dev python3-pip  python3-virtualenv python-numpy python3-numpy python3-picamera python3-pandas python3-rpi.gpio i2c-tools avahi-utils joystick libopenjp2-7-dev libtiff5-dev gfortran libatlas-base-dev libopenblas-dev libhdf5-dev libhdf5-serial-dev libhdf5-103 git ntp
@@ -18,13 +18,13 @@ yes | sudo apt-get install build-essential python3 python3-dev python3-pip  pyth
 yes | sudo apt-get install libilmbase-dev libopenexr-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libjasper-dev libwebp-dev libatlas-base-dev gfortran libeigen3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev v4l-utils libxvidcore-dev libx264-dev libxine2-dev libqtgui4 libqtwebkit4 libqt4-test
 
 	
-sudo apt-get install libgtk-3-dev
-sudo apt-get install mesa-utils libgl1-mesa-dri libgtkgl2.0-dev libgtkglext1-dev
+yes | sudo apt-get install libgtk-3-dev
+yes | sudo apt-get install mesa-utils libgl1-mesa-dri libgtkgl2.0-dev libgtkglext1-dev
 
 
 wget  https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip.py
-sudo pip3 install opencv-contrib-python
+yes | sudo python3 get-pip.py
+yes | sudo pip3 install opencv-contrib-python
 
 #Setup Virtual Env
 python3 -m virtualenv -p python3 env --system-site-packages
