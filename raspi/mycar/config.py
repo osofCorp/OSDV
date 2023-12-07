03bc6d8047550a4bd85842344f7aeec401602dc9
44401213SeonghunPark <41362677+44401213SeonghunPark@users.noreply.github.com>
2023-12-07 13:02:16 +0900

    asd

--- raspi/mycar/config.py
+++ raspi/mycar/config.py
@@ -118,19 +118,6 @@ THROTTLE_STOPPED_PWM = 370      #pwm value for no movement
 THROTTLE_REVERSE_PWM = 220      #pwm value for max reverse throttle
 
 #
-# PIGPIO_PWM (deprecated in favor of PWM_STEERING_THROTTLE)
-#
-TATAMI_STEERING_LEFT_PWM = 2172 #PWM value for full left steering (Center PWM + steering level)
-TATAMI_STEERING_RIGHT_PWM = 916 #PWM value for full right steering
-TATAMI_STEERING_FEEL = 0.3 #Steering Feeling Adjustment (Angle Level at Steering 50%)  
-TATAMI_STEERING_BALANCE = -0.1 #Steering L/R Balance -1.0(L)..+1.0(R)
-TATAMI_THROTTLE_START_BOOST_TIME = 0.9 #Throttle boost time[sec]
-TATAMI_THROTTLE_START_BOOST = 1.0 #Throttle boost level for start torque up(0..1)
-TATAMI_THROTTLE_UPPER_LIMIT = 1.0 #Throttle upper limit (0..1)
-TATAMI_THROTTLE_LOWER_LIMIT = 0.8 #Throttle lower limit (0..1)
-TATAMI_THROTTLE_STEERING_BOOST = 1.0 #Throttle boost adjustment by steering angle (0..1)
-
-#
 # SERVO_HBRIDGE_2PIN
 # - configures a steering servo and an HBridge in 2pin mode (2 pwm pins)
 # - Servo takes a standard servo PWM pulse between 1 millisecond (fully reverse)
